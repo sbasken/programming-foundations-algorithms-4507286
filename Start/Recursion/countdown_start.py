ba797ac0c8efd034a7522676a0a0d2ca360db820
Saki Basken <sbasken@tnsi.com>
2025-02-13 13:38:22 -0800

    Complete recursion and sorting

--- Start/Recursion/countdown_start.py
+++ Start/Recursion/countdown_start.py
@@ -3,7 +3,11 @@
 
 
 def countdown(x):
-    return
-
+    if (x == 0):
+        print("Done!")
+        return
+    else:
+        print(x, "...")
+        countdown(x-1)
 
 countdown(5)
--- Start/Sorting/MergeSort/mergesort_start.py
+++ Start/Sorting/MergeSort/mergesort_start.py
@@ -12,17 +12,35 @@ def mergesort(dataset):
 
         # TODO: recursively break down the arrays
 
+        mergesort(leftarr)
+        mergesort(rightarr)
 
         # TODO: now perform the merging
         i=0 # index into the left array
         j=0 # index into the right array
         k=0 # index into merged array
 
+        while i < len(leftarr) and j < len(rightarr):
+            if leftarr[i] < rightarr[j]:
+                dataset[k] = leftarr[i]
+                i += 1
+            else:
+                dataset[k] = rightarr[j]
+                j += 1
+            k += 1
+
         # TODO: while both arrays have content
 
+        while i < len(leftarr):
+            dataset[k] = leftarr[i]
+            i += 1
+            k += 1
 
         # TODO: if the left array still has values, add them
-
+        while j < len(rightarr):
+            dataset[k] = rightarr[j]
+            j += 1
+            k += 1
 
         # TODO: if the right array still has values, add them
 
